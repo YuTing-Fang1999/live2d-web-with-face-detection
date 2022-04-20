d363e72e0f207ce1389313b34e9df84174afc3c7
YuTing-Fang1999 <s830s200s@gmail.com>
2022-04-20 16:53:29 +0800

    first commit

---
+++ Samples/TypeScript/Demo/socket server.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  origins: '*:*', path: '/',
+  serveClient: false
+});
+
+io.sockets.on('connection', (socket) => {
+  console.log(`socket [${socket.id}] connected`);
+
+  // test sever to client
+  // setInterval(function() {
+  //   socket.emit('date', {'date': new Date()});
+  // }, 1000);
+
+  socket.on('msg', (data) => {
+    // console.log(`socket [${socket.id}] msg`, data);
+    socket.broadcast.emit('jsClient', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`socket [${socket.id}] disconnected`)
+  })
+});
+
+http.listen(5252, () => {
+  console.log('listening on 5252');
+})
